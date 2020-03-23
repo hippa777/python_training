cfeaeaf0e3a61c6aaf36f73c1b910d3e2040cf18
hippa777 <Q1xj9Pm>
2020-03-23 23:06:02 +0400

    Выделен класс для работы с группами

--- test/test_add_group_redfox.py
+++ test/test_add_group_redfox.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group_redfox(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="222", header="333", footer="444"))
+    app.group.create(Group(name="222", header="333", footer="444"))
     app.session.logout()
 
 
 def test_add_empty_group_redfox(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -13,9 +13,9 @@ def app(request):
 
 def test_new_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_new_contact(Contact(firstname="Tata", middlename="Dmitrievna", lastname="Ivanova", nickname="tata",
-                                   company="asd-groop", address="USSR", homephone="111111111",
-                                   mobile="222222222", workphone="333333333", fax="444444444",
-                                   email="tata@tata.ru",
-                                   bday="17", bmonth="January", byear="2000", title="123"))
+    app.contact.create(Contact(firstname="Tata", middlename="Dmitrievna", lastname="Ivanova", nickname="tata",
+                               company="asd-groop", address="USSR", homephone="111111111",
+                               mobile="222222222", workphone="333333333", fax="444444444",
+                               email="tata@tata.ru",
+                               bday="17", bmonth="January", byear="2000", title="123"))
     app.session.logout()
