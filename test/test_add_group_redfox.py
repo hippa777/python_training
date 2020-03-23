070100e8b9207077084d8a487f42608fd1634d2b
hippa777 <Q1xj9Pm>
2020-03-23 22:50:22 +0400

    Выделен класс для управления сесиями

--- test/test_add_group_redfox.py
+++ test/test_add_group_redfox.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_group_redfox(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="222", header="333", footer="444"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group_redfox(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
