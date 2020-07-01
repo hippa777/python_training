3afd2d0795e694bf26cb3c031dd32d037243c5b2
hippa777 <Q1xj9Pm>
2020-07-01 20:46:57 +0400

    Задание №16: Реализовать опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- conftest.py
+++ conftest.py
@@ -5,13 +5,15 @@ web_app = None
 
 
 @pytest.fixture(scope="session")
-def app():
+def app(request):
     global web_app
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if web_app is None:
-        web_app = Application()
+        web_app = Application(browser=browser, base_url=base_url)
     else:
         if not web_app.is_valid():
-            web_app = Application()
+            web_app = Application(browser=browser, base_url=base_url)
 
     web_app.session_helper.ensure_login(username="admin", password="secret")
     return web_app
@@ -25,3 +27,8 @@ def stop(request):
 
     request.addfinalizer(fin)
     return web_app
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http: // localhost / addressbook /")
--- fixture/application.py
+++ fixture/application.py
@@ -6,14 +6,23 @@ from fixture.session_helper import SessionHelper
 
 
 class Application:
-    def __init__(self):
-        webdriver_firefox = webdriver.Firefox()
-        self.wd = webdriver_firefox
+    def __init__(self, browser, base_url):
+        if browser == "Firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "Chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "Ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        # webdriver_firefox = webdriver.Firefox()
+        # self.wd = webdriver_firefox
         self.wd.implicitly_wait(30)
         # инициализируем классы для работы с функциональностью приложения
         self.session_helper = SessionHelper(self.wd)
         self.contact_helper = ContactHelper(self.wd)
         self.group_helper = GroupHelper(self.wd)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -21,6 +30,11 @@ class Application:
             return True
         except:
             return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
