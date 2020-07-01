77b18619ebcfc7c50c5912aa1aebd2d8acc07504
hippa777 <Q1xj9Pm>
2020-07-01 18:50:48 +0400

    Задание №15: Параметризовать тесты для создания групп и контактов

--- test/test_add_group_redfox.py
+++ test/test_add_group_redfox.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
-def test_add_group_redfox(app):
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits+string.punctuation + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+    for i in range(5)]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group_redfox(app, group):
     old_groups = app.group_helper.get_group_list()
-    group = Group(name="222", header="333", footer="444")
-    app.group_helper.create(group)
-    assert len(old_groups)+1 == app.group_helper.count()
-    new_groups = app.group_helper.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-def test_add_empty_group_redfox(app):
-    old_groups = app.group_helper.get_group_list()
-    group = Group(name="", header="", footer="")
     app.group_helper.create(group)
     assert len(old_groups)+1 == app.group_helper.count()
     new_groups = app.group_helper.get_group_list()
--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -1,13 +1,43 @@
 from model.contact import Contact
+import pytest
+import random
+import string
+from random import randint
 
 
-def test_new_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_phone():
+    symbols = string.digits
+    first = random.randint(1, 9)
+    return str(first) + ''.join([random.choice(symbols)*8])
+
+
+def random_email():
+    symbols = string.ascii_letters + string.digits
+    return "".join([random.choice(symbols) for i in range(random.randrange(3, 7, 1))]) + "@" + "".join(
+        [random.choice(symbols) for i in range(random.randrange(3, 7, 1))]) + "." + "".join(
+        [random.choice(symbols) for i in range(random.randrange(3, 7, 1))])
+
+
+testdata = [
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 15),
+            lastname=random_string("lastname", 15), nickname=random_string("nickname", 10),
+            company=random_string("company", 20), address=random_string("address", 20),
+            home=random_phone(),
+            mobile=random_phone(), work=random_phone(), fax=random_phone(), email=random_email(),
+            email2=random_email(), email3=random_email(),
+            address2=random_string("address2", 15)) for i in
+    range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_new_contact(app, contact):
     old_contacts = app.contact_helper.get_contact_list()
-    contact = Contact(firstname="Tata", middlename="Dmitrievna", lastname="Ivanova", nickname="tata",
-                      company="asd-groop", address="USSR", home="111111111",
-                      mobile="222222222", work="333333333", fax="444444444",
-                      email="tata@tata.ru",
-                      bday="17", bmonth="January", byear="2000", title="123")
     app.contact_helper.create(contact)
     n = app.contact_helper.count()
     assert len(old_contacts) + 1 == n
